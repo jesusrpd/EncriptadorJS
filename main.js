a6707a1d8f332a77abc19f246f0469a202f6cca4
jesusrpd <jesusrodrigo881@gmail.com>
2022-01-19 07:56:46 -0600

    functionality encrypt

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const copy = document.getElementById("btn-copy");
+const btnEncrypt = document.querySelector('.btn-encriptar');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
 
 const copyMessage = () => {
     console.log('message copied');
 }
 
+const encrypt = e => {
+    e.preventDefault();
+    let arrayString = input.value.split('');
+    const encryptMessage = arrayString.map( l => l === 'a' ? 'ai': l === 'i' ? 'imes': l === 'e' ? 'enter' : l === 'o' ? 'ober': l === 'u' ? 'ufat': l);
+    output.innerHTML = encryptMessage.join('');
+}
+
+
+
 copy.addEventListener('click', copyMessage);
+btnEncrypt.addEventListener('click', encrypt);
